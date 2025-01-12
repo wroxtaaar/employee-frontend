2df195b178d9bc7fd8786f72ae7784d3ac1ce2b3
wasiq <wroxtaar@gmail.com>
2025-01-12 19:30:07 +0530

    Initial commit

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,15 @@
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className='navbar navbar-dark bg-dark'>
+                <button className="navbar-brand btn btn-link" type="button">Employee Management System</button>
+            </nav>
+        </header>
+
+    </div>
+  )
+}
+
+export default HeaderComponent
