4043778e376ab38b75ac331a136cd329b85b65ae
Abdul Wasiq <71366336+wroxtaaar@users.noreply.github.com>
2025-11-07 10:21:26 +0000

    test

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const REST_API_BASE_URL =
   import.meta.env.VITE_API_URL ||
-  "http://localhost:8080/api/employees";  
+  "https://employee-backend-9cgj.onrender.com/api/employees";  
 
 export const listEmployees = () => axios.get(REST_API_BASE_URL);
 
