e7568781c91335bf47205a6947c539b5bf01830a
Abdul Wasiq <71366336+wroxtaaar@users.noreply.github.com>
2025-11-09 12:02:37 +0000

    added local host

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,17 +1,41 @@
+// import axios from "axios";
+
+// const REST_API_BASE_URL =
+//   "http://localhost:8080/api/employees" ||
+//   "https://employee-backend-9cgj.onrender.com/api/employees";
+
+// export const listEmployees = () => axios.get(REST_API_BASE_URL);
+
+// export const createEmployee = (employee) =>
+//   axios.post(REST_API_BASE_URL + "/create", employee);
+
+// export const getEmployee = (employeeId) =>
+//   axios.get(REST_API_BASE_URL + "/" + employeeId);
+
+// export const updateEmployee = (employeeId, employee) =>
+//   axios.put(REST_API_BASE_URL + "/update/" + employeeId, employee);
+
+// export const deleteEmployee = (employeeId) =>
+//   axios.delete(REST_API_BASE_URL + "/delete/" + employeeId);
+
 import axios from "axios";
 
-const REST_API_BASE_URL = "https://employee-backend-9cgj.onrender.com/api/employees";  
+const LOCAL = "http://localhost:8080/api/employees";
+const PROD = "https://employee-backend-9cgj.onrender.com/api/employees";
 
-export const listEmployees = () => axios.get(REST_API_BASE_URL);
+const api = axios.create({ baseURL: LOCAL });
 
-export const createEmployee = (employee) =>
-  axios.post(REST_API_BASE_URL + "/create", employee);
+// If localhost fails, switch to production
+api.interceptors.response.use(
+  (res) => res,
+  () => {
+    api.defaults.baseURL = PROD;
+    return api;
+  }
+);
 
-export const getEmployee = (employeeId) =>
-  axios.get(REST_API_BASE_URL + "/" + employeeId);
-
-export const updateEmployee = (employeeId, employee) =>
-  axios.put(REST_API_BASE_URL + "/update/" + employeeId, employee);
-
-export const deleteEmployee = (employeeId) =>
-  axios.delete(REST_API_BASE_URL + "/delete/" + employeeId);
+export const listEmployees = () => api.get("");
+export const createEmployee = (employee) => api.post("/create", employee);
+export const getEmployee = (id) => api.get(`/${id}`);
+export const updateEmployee = (id, employee) => api.put(`/update/${id}`, employee);
+export const deleteEmployee = (id) => api.delete(`/delete/${id}`);
